824d5124eca039746deb280f39a15990000754b9
rzilla92 <108125593+rzilla92@users.noreply.github.com>
2022-11-14 16:56:21 +0700

    Update Portfolio.js

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -13,8 +13,8 @@ export default class Porfolio extends Component {
               return(
                 <div className="columns portfolio-item">
                   <div className="item-wrap">
-                    <a target="_blank" rel="nonreferrer" href={`${item.purl}`}>
-                      <img src={`${item.imgurl}`} className="item-img"/>
+                    <a target="_blank" rel="noreferrer" href={`${item.purl}`}>
+                      <img src={`${item.imgurl}`} className="item-img" alt={item.name}/>
                         <div className="portfolio-item-meta">
                           <h5>{item.name}</h5>
                           <p>{item.description}</p>
